309b5902b1d6fd5c43357f3253bcde0876d8b531
Mary <masha121105@gmail.com>
2026-05-08 23:00:32 +0700

    многопоточность

--- server.c
+++ server.c
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 
 #define PORT 8888
 
+void *handle_client(void *arg){
+    int client_fd = *(int*)arg;
+    free(arg);
+    printf("Клиент обрабатывается в потоке\n");
+    close(client_fd);
+    return NULL;
+}
+
 int main(){
-    int server_fd, client_fd;
+    int server_fd, *client_fd;
     struct sockaddr_in addr;
     socklen_t addr_len = sizeof(addr);
+    pthread_t thread;
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     addr.sin_family = AF_INET;
@@ -21,10 +31,14 @@ int main(){
 
     printf("Сервер слушает порт %d\n", PORT);
 
-    client_fd = accept(server_fd, (struct sockaddr*)&addr, &addr_len);
-    printf("Клиент подключился\n");
+    while(1){
+        client_fd = malloc(sizeof(int));
+        *client_fd = accept(server_fd, (struct sockaddr*)&addr, &addr_len);
 
-    close(client_fd);
+        printf("Клиент подключился\n");
+        pthread_create(&thread, NULL, handle_client, client_fd);
+        pthread_detach(thread);
+    }
     close(server_fd);
 
     return 0;
